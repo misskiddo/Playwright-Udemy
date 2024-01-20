9e1e2ed1ef04e1ddfc14be28dcf3eaa61ebc452c
Toni Dell Home <misskiddo@hotmail.com>
2024-01-20 17:25:12 +0100

    Child and Parent elements

--- tests/01Locatorst.spec.ts
+++ tests/01Locatorst.spec.ts
@@ -1,44 +1,114 @@
-import {test} from '@playwright/test'
+import { test } from "@playwright/test";
 
-test.beforeEach(async({page})=>{
-    await page.goto('http://localhost:4200');
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click()
-})
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
+});
 
-test.describe('Locator syntax rules', ()=>{
-    test('Locators', async ({page})=>{
-       // by Tag name
-       page.locator('input')  
+test.describe("Locator syntax rules", () => {
+  test("Locators", async ({ page }) => {
+    // by Tag name
+    page.locator("input");
 
-       //by Id
-       page.locator('#inputEmail1')
+    //by Id
+    page.locator("#inputEmail1");
 
-       //by Class name
-       page.locator('.shape-rectangle')
+    //by Class name
+    page.locator(".shape-rectangle");
 
-       //by attribute
-       page.locator('[placeholder="Email"]')
+    //by attribute
+    page.locator('[placeholder="Email"]');
 
-       //by class value (full)
-       page.locator('class="input-full-width size-medium status-basic shape-rectangle nb-transition"')
+    //by class value (full)
+    page.locator(
+      'class="input-full-width size-medium status-basic shape-rectangle nb-transition"',
+    );
 
-       //combine different selectors
-       page.locator('input[placeholder="Email"].shape-rectangle')
+    //combine different selectors
+    page.locator('input[placeholder="Email"].shape-rectangle');
 
-       //by XPath (NOT RECOMMENDED)
-       page.locator('//*[@id="inputEmail1"]')
+    //by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]');
 
-       //by partial text match
-       page.locator(':text("Using)')
+    //by partial text match
+    page.locator(':text("Using)');
 
-       // by exact text match
-       page.locator(':text-is("Using the Grid")')
+    // by exact text match
+    page.locator(':text-is("Using the Grid")');
+  });
 
+  test("User Facing Locators", async ({ page }) => {
+    await page.getByRole("button", { name: "Sign in" }).first().click();
 
-    })
+    await page.getByLabel("Email").first().click();
 
-    
-})
+    await page.getByPlaceholder("Jane Doe").first().click();
 
+    await page.getByText("Using the Grid").first().click();
 
+    await page.getByTestId("SignIn").first().click();
+
+    await page.getByTitle("IoT Dashboard").first().click();
+  });
+
+  test("Child elements", async ({ page }) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+    await page
+      .locator("nb-card")
+      .locator("nb-radio")
+      .locator(':text-is("Option 2")')
+      .click();
+
+    // Click on "Sign In" under "Using the Grid"
+    await page
+      .locator("nb-card")
+      .getByRole("button", { name: "Sign in" })
+      .first()
+      .click();
+    await page.locator("nb-card").nth(2).getByRole("button").click();
+  });
+
+  test("Parent elements", async ({ page }) => {
+    // Click on "Email" under "Using the Grid"
+    await page
+      .locator("nb-card", { hasText: "Using the Grid" })
+      .getByRole("textbox", { name: "Email" })
+      .click();
+
+    // Click on "radio button Option 2" on the form that contains Email1
+    await page
+      .locator("nb-card", { has: page.locator("#inputEmail1") })
+      .locator(':text-is("Option 2")')
+      .click();
+
+    // Click on "Email" under "Basic form"
+    await page
+      .locator("nb-card")
+      .filter({ hasText: "Basic form" })
+      .getByRole("textbox", { name: "Email" })
+      .click();
+
+    // Click on "Submit" on the Basic form
+    await page
+      .locator("nb-card")
+      .filter({ has: page.locator(".status-danger") })
+      .getByRole("button")
+      .click();
+
+    // Filter those nb-card that has a checkbox and 'Sign in' button, and finally click on the Email field
+    await page
+      .locator("nb-card")
+      .filter({ has: page.locator("nb-checkbox") })
+      .filter({ hasText: "Sign in" })
+      .getByRole("textbox", { name: "Email" })
+      .click();
+      
+    // Using xpath parent '..'
+    await page
+      .locator(':text-is("Using the Grid")')
+      .locator("..")
+      .getByRole("textbox", { name: "Email" })
+      .click();
+  });
+});
